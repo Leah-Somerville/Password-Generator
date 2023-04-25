089145cfe8480dc5eac2d6dded5ca219a8020dda
Leah Somerville <leah.somerville4@gmail.com>
2023-04-25 10:03:19 -0500

    Character generator functions created.

---
+++ app.js
@@ -0,0 +1,50 @@
+// Password Generator
+
+// Character Generator Function
+
+//Function that accepts a string value as an argument and returns a random index number from the string argument
+function randomIndex(str){
+    return Math.floor(Math.random() * str.length);
+}
+
+// Example of the randomIndex function
+console.log(randomIndex(`chicken`)); // 0,1,2,3,4,5,6
+
+
+// Function that returns a random lowercase letter using randomIndex in the "letters" string
+function getRandomLower(){
+    const letters = `abcdefghijklmnopqrstuvwxyz`;
+
+    // Returning a random letter by using randomIndex
+    return letters[randomIndex(letters)];
+}
+// Example of getRandomLower
+console.log(getRandomLower());
+
+// Function that returns a random uppercase letter using randomIndex in the "letters" string
+function getRandomUpper(){
+    // Running the getRandomLower function to create a random lowercase letter and then changing the letter to uppercase
+    return getRandomLower().toUpperCase();
+}
+
+console.log(getRandomUpper());
+
+// Function that returns a random number (AKA Random number as a string value)
+function getRandomNumber(){
+    const numbers = `1234567890`;
+    // Returning a random number using a random index from the "numbers" string
+    return numbers[randomIndex(numbers)];
+}
+  
+// Example of the getRandomNumber function
+console.log(getRandomNumber()); // Random number from the "numbers" string
+
+// Function that returns a random symbol
+function getRandomSymbol(){
+    const symbols = `!@#$%^&*(){}[]=<>/,.`;
+    // Returning a random symbol using a random index from the "symbols" string
+    return symbols[randomIndex(symbols)];
+}
+  
+// Example of the getRandomSymbol function
+console.log(getRandomSymbol()); // Random symbol from the "symbols" string
